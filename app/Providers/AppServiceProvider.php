45e146830a1dd914c8dfdb4a382d9b9986ea2103
hanifbinz <hanifbinz@gmail.com>
2026-06-15 09:27:27 +0700

    update security

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Baris ini wajib ada untuk memanggil fungsi URL
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Memaksa Laravel menggunakan HTTPS HANYA jika berada di server Production (VPS)
+        // Agar saat Anda coding di lokal (Laragon) tanpa SSL, aplikasi tidak error.
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
